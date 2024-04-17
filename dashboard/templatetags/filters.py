de27bd20eab4fa0c3ea3c9277d6fcd60ca472bcf
R1ckyH <836650357ho@gmail.com>
2024-04-18 03:46:04 +0800

    update css and html

--- dashboard/templatetags/filters.py
+++ dashboard/templatetags/filters.py
@@ -4,3 +4,11 @@ register = template.Library()
 @register.filter
 def indexed(indexable, i):
     return indexable[i - 1]
+
+
+@register.filter
+def rounded(data, d=2):
+    if type(data) == float:
+        return round(data, d)
+    else:
+        return data
