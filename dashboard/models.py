d0fdf6a33ccec2053f924679f143f890ea4b39d4
R1ckyH <836650357ho@gmail.com>
2024-04-17 21:32:53 +0800

    update views and models

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,126 +1,12 @@
 from django.db import models
-from django.db import models
 
 
-class Duxu1000009(models.Model):
+class DriveData(models.Model):
     driverID = models.CharField(max_length=100)
     carPlateNumber = models.CharField(max_length=100)
     unix_Time = models.BigIntegerField()
     speed = models.FloatField()
     isOverspeed = models.IntegerField()
-
-    class Meta:
-        db_table = 'duxu1000009'
-
-
-class Hanhui1000002(models.Model):
-    driverID = models.CharField(max_length=100)
-    carPlateNumber = models.CharField(max_length=100)
-    unix_Time = models.BigIntegerField()
-    speed = models.FloatField()
-    isOverspeed = models.IntegerField()
-
-    class Meta:
-        db_table = 'hanhui1000002'
-
-
-class Haowei1000008(models.Model):
-    driverID = models.CharField(max_length=100)
-    carPlateNumber = models.CharField(max_length=100)
-    unix_Time = models.BigIntegerField()
-    speed = models.FloatField()
-    isOverspeed = models.IntegerField()
-
-    class Meta:
-        db_table = 'haowei1000008'
-
-
-class Likun1000003(models.Model):
-    driverID = models.CharField(max_length=100)
-    carPlateNumber = models.CharField(max_length=100)
-    unix_Time = models.BigIntegerField()
-    speed = models.FloatField()
-    isOverspeed = models.IntegerField()
-
-    class Meta:
-        db_table = 'likun1000003'
-
-
-class Panxian1000005(models.Model):
-    driverID = models.CharField(max_length=100)
-    carPlateNumber = models.CharField(max_length=100)
-    unix_Time = models.BigIntegerField()
-    speed = models.FloatField()
-    isOverspeed = models.IntegerField()
-
-    class Meta:
-        db_table = 'panxian1000005'
-
-
-class Shenxian1000004(models.Model):
-    driverID = models.CharField(max_length=100)
-    carPlateNumber = models.CharField(max_length=100)
-    unix_Time = models.BigIntegerField()
-    speed = models.FloatField()
-    isOverspeed = models.IntegerField()
-
-    class Meta:
-        db_table = 'shenxian1000004'
-
-
-class Xiexiao1000001(models.Model):
-    driverID = models.CharField(max_length=100)
-    carPlateNumber = models.CharField(max_length=100)
-    unix_Time = models.BigIntegerField()
-    speed = models.FloatField()
-    isOverspeed = models.IntegerField()
-
-    class Meta:
-        db_table = 'xiexiao1000001'
-
-
-class Xiezhi1000006(models.Model):
-    driverID = models.CharField(max_length=100)
-    carPlateNumber = models.CharField(max_length=100)
-    unix_Time = models.BigIntegerField()
-    speed = models.FloatField()
-    isOverspeed = models.IntegerField()
-
-    class Meta:
-        db_table = 'xiezhi1000006'
-
-
-class Zengpeng1000000(models.Model):
-    driverID = models.CharField(max_length=100)
-    carPlateNumber = models.CharField(max_length=100)
-    unix_Time = models.BigIntegerField()
-    speed = models.FloatField()
-    isOverspeed = models.IntegerField()
-
-    class Meta:
-        db_table = 'zengpeng1000000'
-
-
-class Zengpeng1000000(models.Model):
-    driverID = models.CharField(max_length=100)
-    carPlateNumber = models.CharField(max_length=100)
-    unix_Time = models.BigIntegerField()
-    speed = models.FloatField()
-    isOverspeed = models.IntegerField()
-
-    class Meta:
-        db_table = 'zengpeng1000000'
-
-
-class Zouan1000007(models.Model):
-    driverID = models.CharField(max_length=100)
-    carPlateNumber = models.CharField(max_length=100)
-    unix_Time = models.BigIntegerField()
-    speed = models.FloatField()
-    isOverspeed = models.IntegerField()
-
-    class Meta:
-        db_table = 'zouan1000007'
 
 
 class Summary(models.Model):
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,18 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 from .models import Summary
-from .models import (
-    Duxu1000009,
-    Hanhui1000002,
-    Haowei1000008,
-    Likun1000003,
-    Panxian1000005,
-    Shenxian1000004,
-    Xiexiao1000001,
-    Xiezhi1000006,
-    Zengpeng1000000,
-    Zouan1000007,
-)
+from .models import DriveData
 
 from django.db.models import Sum
 
@@ -57,28 +46,7 @@ def monitor_data(request):
     driverID = request.GET.get("driverID")
     time = int(request.GET.get("time"))
 
-    if driverID == "duxu1000009":
-        model = Duxu1000009.objects
-    elif driverID == "hanhui1000002":
-        model = Hanhui1000002.objects
-    elif driverID == "haowei1000008":
-        model = Haowei1000008.objects
-    elif driverID == "likun1000003":
-        model = Likun1000003.objects
-    elif driverID == "panxian1000005":
-        model = Panxian1000005.objects
-    elif driverID == "shenxian1000004":
-        model = Shenxian1000004.objects
-    elif driverID == "xiexiao1000001":
-        model = Xiexiao1000001.objects
-    elif driverID == "xiezhi1000006":
-        model = Xiezhi1000006.objects
-    elif driverID == "zengpeng1000000":
-        model = Zengpeng1000000.objects
-    elif driverID == "zouan1000007":
-        model = Zouan1000007.objects
-
-    related_data = model.filter(unix_Time__gte=(time - 30), unix_Time__lte=time).order_by('unix_Time').values(
+    related_data = DriveData.objects.filter(unix_Time__gte=(time - 30), unix_Time__lte=time, driverID=driverID).order_by('unix_Time').values(
         'unix_Time', 'speed', 'isOverspeed')
 
     return_payload = {
